3406c7cd67356242e53b0386ca6b2d85fe02729a
drunkkiwi <45375635+drunkkiwi@users.noreply.github.com>
2018-12-27 23:56:49 +0100

    Added login and created auth views

--- uakers/home/urls.py
+++ uakers/home/urls.py
@@ -6,9 +6,10 @@ app_name = "home"
 
 urlpatterns = [
     path('', views.home_view, name="home_view"),
-    path('co_post', views.confession_submit, name='co_post'),
+    path('co_post', views.confession_post_view, name='confession_post_view'),
 
     #auth views
-    path('a/register', views.register, name='register'),
-    path('a/logout', auth_views.LogoutView.as_view(next_page="/"), name='log_out'),
+    path('a/login', auth_views.LoginView.as_view(template_name="home/log_in.html"), name='log_in_view'),
+    path('a/signup', views.sign_up_view, name='sign_up_view'),
+    path('a/logout', views.log_out_view, name='log_out_view'),
 ]
--- uakers/home/views.py
+++ uakers/home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts       import render, redirect
 from .models                import Confessions
 from .forms                 import SignUpForm, ConfessionsForm
-from django.contrib.auth    import login, authenticate
+from django.contrib.auth    import login, authenticate, logout
 
 
+# -------------- HOME VIEW ----------------------------
 def home_view(request):
     all_conf = Confessions.objects.all().order_by('-id')
 
@@ -15,8 +16,24 @@ def home_view(request):
     }
     return render(request, 'home/home.html', context)
 
+# ------------- CONFESSION POST FORM VIEW -----------------
+def confession_post_view(request):
+    co_form = ConfessionsForm(request.POST)
+    if co_form.is_valid():
+        temp = co_form.save(commit=False)
+        temp.confession_author = request.user
+        temp.save()
+        return redirect('home:home_view')
 
-def register(request):
+    return redirect('home:home_view')
+
+# ------------ AUTHENTICATION VIEWS ------------------------
+
+#-------------- LOGIN VIEW ---------------------------------
+
+# ---------------- SIGN UP VIEW ----------------------------
+def sign_up_view(request):
+    sign_up_btn = 'Sign up'
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -31,16 +48,13 @@ def register(request):
 
     context = {
         'form': form,
+        'sign_up_btn': sign_up_btn,
     }
     return render(request, 'home/sign_up.html', context)
 
 
-def confession_submit(request):
-    co_form = ConfessionsForm(request.POST)
-    if co_form.is_valid():
-        temp = co_form.save(commit=False)
-        temp.confession_author = request.user
-        temp.save()
-        return redirect('home:home_view')
 
+#------------------- LOG OUT VIEW ------------------------
+def log_out_view(request):
+    logout(request)
     return redirect('home:home_view')
